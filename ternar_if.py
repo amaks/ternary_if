23c4183786d47572c768bd7dc608fbb43a6f66be
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-10-01 11:46:56 +0300

    update

--- ternar_if.py
+++ ternar_if.py
@@ -7,43 +7,57 @@ class TernarIfCommand(sublime_plugin.TextCommand):
       selected_area = self.view.substr(region)
 
       if 'if' in selected_area:
-        split         = selected_area.split('\n')
-        condition     = split[0].replace('if', '').replace('\n', '').strip()
-        equal         = split[1].split('=')[0].replace('\n', '').strip()
-        first_result  = split[1].split('=')[1].replace('\n', '').strip()
-        second_result = split[3].split('=')[1].replace('\n', '').strip()
 
-        new_line    = ''
-        new_line    += equal + ' = '
-        new_line    += condition + ' ? '
-        new_line    += first_result
-        new_line    += ' : '
-        new_line    += second_result
+        if ' = ' in selected_area:
+          split         = selected_area.split('\n')
+          condition     = split[0].replace('if', '').replace('\n', '').strip()
+          equal         = split[1].split(' = ')[0].replace('\n', '').strip()
+          first_result  = split[1].split(' = ')[1].replace('\n', '').strip()
+          second_result = split[3].split(' = ')[1].replace('\n', '').strip()
+
+          new_line    = ''
+          new_line    += equal + ' = '
+          new_line    += condition + ' ? '
+          new_line    += first_result
+          new_line    += ' : '
+          new_line    += second_result
+        else:
+          edited_line = selected_area.replace('if ', '').replace('else', ':').replace('end', '')
+          split       = edited_line.split('\n')
+          new_line    = ''
+
+          for i in split:
+            if ' == ' in i:
+              new_line += i + '?'
+            else:
+              new_line += ' ' + i.strip()
+
       else:
         split       = selected_area.split('?')
         first_part  = split[0]
         startrow, startcol = self.view.rowcol(region.begin())
         spaces    = [" " for x in range(startcol)]
 
-        if '=' in first_part:
-          equal      = first_part.split('=')[0]
-          first_part = first_part.split('=')[1]
+        if ' = ' in first_part:
+          equal      = first_part.split(' = ')[0]
+          first_part = first_part.split(' = ')[1]
 
         second_part = split[1].split(':')[0]
         third_part  = split[1].split(':')[1]
 
-        new_line    = 'if'
+        new_line    = 'if '
         new_line += first_part + '\n'
 
         if 'equal' in locals():
-          new_line += ''.join(spaces) + '  ' + equal + '='
+          new_line += ''.join(spaces) + '  ' + equal + ' ='
         new_line += second_part + '\n'
 
         new_line += ''.join(spaces) + 'else\n'
 
         if 'equal' in locals():
-          new_line += ''.join(spaces) + '  ' + equal + '='
+          new_line += ''.join(spaces) + '  ' + equal + ' ='
 
         new_line += third_part + '\n'
+        new_line += 'end\n'
 
       self.view.replace(edit, region, new_line)
